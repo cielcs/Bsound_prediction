fd5a5538a97e1bd882c323244851f1ca22dcf6df
cielcs <shotalicewww2@gmail.com>
2024-08-28 17:45:19 +0900

    priorandmarginallikelihoodposterior

--- calc_posterior.py
+++ calc_posterior.py
@@ -39,11 +39,23 @@ new_mfcc_pca = new_mfcc_pca.flatten()
 
 #古典的に正規分布をそれぞれ仮定して、その分布に対する尤度を計算
 likelihoods2 = {}
+first_prior = {}
+Marginal_likelihood = 0
+Posterior = {}
 for group_key, distribution in sorted_group_distributions.items():
     mean = distribution['mean']
     cov = distribution['cov']
     likelihood2 = np.exp(-0.5 * (new_mfcc_pca - mean).T @ np.linalg.inv(cov) @ (new_mfcc_pca - mean)) / ((2*np.pi)**(len(mean)/2)*np.sqrt(np.linalg.det(cov)))
     likelihoods2[group_key] = likelihood2
+    first_prior[group_key] = 1/len(sorted_group_distributions)
+    Marginal_likelihood += likelihood2 * first_prior[group_key]
+
+for(group_key, distribution) in sorted_group_distributions.items():
+    Posterior[group_key] = likelihoods2[group_key] * first_prior[group_key] / Marginal_likelihood
+
 # print("Likelihoods:", likelihoods)
 print("Likelihoods2:", likelihoods2)
+print("First prior:", first_prior)
+print("Marginal likelihood:", Marginal_likelihood)
+print("Posterior:", Posterior)
 # print(sorted_group_distributions.keys())
